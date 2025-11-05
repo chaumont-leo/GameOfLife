4a29b5cb668aa5ac4cc1848266f2ec6f406e7564
Léo Chaumont <leo.chaumont15@gmail.com>
2025-11-05 13:59:23 +0100

    wip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,45 @@
 import processing.core.PApplet;
+import java.util.Random;
+
+import java.util.Random;
 
 public class Main extends PApplet {
+    int cellSize = 4;
+    int height = 800;
+    int width = 800;
+    boolean[][] grid;
 
     public static void main(String[] args) {
         PApplet.main("Main");
-
     }
 
     public void settings() {
-        size(800, 600);
-    }
-    public void draw() {
-        text("Hello world!", width / 2, height / 2);
-    }
-    public void setup() {
-        textSize(32);
-        textAlign(CENTER);
-        background(255,155,255);
+        size(width, height);
     }
 
+    public void draw() {
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                if (grid[x][y]) {
+                    rect(x * cellSize, y * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+    }
+    public void setup() {
+        stroke(255);  // Set line drawing color to white
+        background(0,0,0);
+        this.grid = randomize(height / cellSize, width / cellSize);
+    }
+
+    private static boolean[][] randomize(int rows, int cols) {
+        Random random = new Random();
+        boolean[][] array = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = random.nextBoolean();
+            }
+        }
+        return array;
+    }
 }
---
+++ src/main/java/model/Cell.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Cell {
+    private int row;
+    private int col;
+}
